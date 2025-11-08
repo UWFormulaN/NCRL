b705be4327fe399167dc4c45603601262afe9379
Ishaan Mittal <123089344+IshaanMittal07@users.noreply.github.com>
2025-11-08 18:15:37 -0500

    Transformation Subsystem Finished (#2)
    * Transformation Subsystem Finished
    
    * Pytest stuff, idk if it works
    
    * Fixed formatting issues with unit testing
    
    * Changed requirements.txt
    
    * Fixed Molecule.py I will fix the test file later
    
    * Nvm I can't sleep if I don't get this done so lets do it!
    
    * Please work
    
    * Added changes after review
    
    * Fixed test issues
    
    * Made changes from last meeting (changed translate paramter to a numpy array, updated typechecking for integers aswell)
    
    * Fixed unittest problems
    
    * Fixed issues highlted in review

--- src/ncrl/Molecule.py
+++ src/ncrl/Molecule.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 
 class Molecule:
 
@@ -49,4 +50,65 @@ class Molecule:
         
     def getContent(self) -> str:
         return self.positions.to_string(header=False, index=False)
+
+    def translate(self, vector:np.ndarray) -> None: 
+        """ 
+        Translates the given molecule X, Y, and Z units 
+
+        Parameters: 
+            vector(np.ndarray): Vector that stores molecule's X, Y, and Z coordinates 
+        """
+
+        if not isinstance(vector, np.ndarray):
+            raise TypeError("Input must be a NumPy array.")
         
+        if not isinstance((vector[0]), (int, float)):
+            raise TypeError(f"{vector[0]} must be an int or float")
+        
+        if not isinstance((vector[1]), (int, float)):
+            raise TypeError(f"{vector[1]} must be an int or float")
+
+        if not isinstance((vector[2]), (int, float)):
+            raise TypeError(f"{vector[2]} must be an int or float")
+
+        self.positions["X"] += vector[0] 
+        self.positions["Y"] += vector[1]
+        self.positions["Z"] += vector[2] 
+        
+    def rotate(self, rotation_matrix:np.ndarray) -> None:
+        """ 
+        Rotates the given molecule X, Y, and Z units 
+
+        Parameters: 
+            rotation_matrix(np.ndarray): 3x3 rotation matrix
+        """
+        if rotation_matrix.shape != (3,3):
+            raise ValueError("The rotation matrix must be a 3x3 matrix")
+        
+        coords = self.positions[["X", "Y", "Z"]].values 
+        rotated = coords @ rotation_matrix.T  #Transpose the matrix to ensure correct shape
+
+        self.positions[["X", "Y", "Z"]] = rotated
+
+    def scale(self, X:float, Y:float, Z:float) -> None: 
+        """ 
+        Scales the X, Y, Z coordintes of the given molecule
+
+        Parameters: 
+            X(float): X scale factor
+            Y(float): Y scale factor
+            Z(float): Z scale factor
+        """
+        if not isinstance((X), (int, float)):
+            raise TypeError(f"{X} must be an int or float")
+        
+        if not isinstance((Y), (int, float)):
+            raise TypeError(f"{Y} must be an int or float")
+
+        if not isinstance((Z), (int, float)):
+            raise TypeError(f"{Z} must be an int or float")
+                
+        self.positions["X"] *= X
+        self.positions["Y"] *= Y
+        self.positions["Z"] *= Z
+    
--- tests/test_Molecule.py
+++ tests/test_Molecule.py
@@ -1,5 +1,6 @@
 import unittest
 import re
+import numpy as np
 from ncrl import Molecule
 
 class MolecueTest(unittest.TestCase):
@@ -8,6 +9,7 @@ class MolecueTest(unittest.TestCase):
 
         self.name = "Propane"
         self.filePath = "tests/Resources/Propane.xyz"
+        self.molecule = Molecule(self.name, self.filePath)
 
         self.columns = ["Atom", "X", "Y", "Z"]
         self.atomNum = 11
@@ -65,3 +67,67 @@ class MolecueTest(unittest.TestCase):
 
         for line in lines:
             self.assertRegex(line.strip(), pattern)
+    
+    def test_translate(self):
+        """ 
+        Asserts if the method returns the intended output based on the test values
+        Note: np.allclose is used due to the inpredictability of float rounding
+        """
+        inital_positions = self.molecule.positions.copy()
+        self.molecule.translate(np.array([1.0, -1.0, 2.0])) 
+        self.assertTrue(np.allclose(self.molecule.positions["X"].values, inital_positions["X"].values + 1.0))
+        self.assertTrue(np.allclose(self.molecule.positions["Y"].values, inital_positions["Y"].values - 1.0))
+        self.assertTrue(np.allclose(self.molecule.positions["Z"].values, inital_positions["Z"].values + 2.0))
+
+    def test_translate_invalid(self):
+        """ 
+        Checks if the invalid inputs are properly being catched with the TypeError
+        """
+        with self.assertRaises(TypeError):
+            self.molecule.translate(np.array(["", -1.0, 2.0]))
+        with self.assertRaises(TypeError):
+            self.molecule.translate(np.array([1.0, "a", 2.0]))
+        with self.assertRaises(TypeError):
+            self.molecule.translate(np.array([1.0, -1.0, " "]))
+
+    def test_rotate(self): 
+        """ 
+        Asserts if the method returns the intended output based on the test values
+        Note: In this an identity matrix is used because it is easiest to test, as it should not change the output
+        Note: np.allclose is used due to the inpredictability of float rounding
+        """
+        inital_positions = self.molecule.positions.copy()
+        rotation_matrix = np.eye(3) #Identity matrix 
+        self.molecule.rotate(rotation_matrix)
+        self.assertTrue(np.allclose(self.molecule.positions[["X", "Y", "Z"]], inital_positions[["X", "Y", "Z"]]))
+
+    def test_rotate_invalid(self):
+        """ 
+        Checks if the invalid inputs are properly being catched with the ValueError
+        Note: In this case, an illogical shape is entered (you cannot do matrix vector product between a 2x2 matrix and 3D vector)
+        """
+        with self.assertRaises(ValueError):
+            self.molecule.rotate(np.ones((2, 2))) #tests for invalid shape
+
+    def test_scale(self):
+        """ 
+        Asserts if the method returns the intended output based on the test values
+        Note: np.allclose is used due to the inpredictability of float rounding
+        """
+        inital_positions = self.molecule.positions.copy()
+        self.molecule.scale(2.0, 3.0, 4.0) 
+        self.assertTrue(np.allclose(self.molecule.positions["X"].values, inital_positions["X"].values * 2.0))
+        self.assertTrue(np.allclose(self.molecule.positions["Y"].values, inital_positions["Y"].values * 3.0))
+        self.assertTrue(np.allclose(self.molecule.positions["Z"].values, inital_positions["Z"].values * 4.0))
+
+    def test_scale_invalid(self):
+        """ 
+        Checks if the invalid inputs are properly being catched with the TypeError
+        """
+        with self.assertRaises(TypeError):
+            self.molecule.scale("", 2.0, 3.0)
+        with self.assertRaises(TypeError):
+            self.molecule.scale(1.0, "a", 3.0)
+        with self.assertRaises(TypeError):
+            self.molecule.scale(1.0, 2.0, " ")
+
